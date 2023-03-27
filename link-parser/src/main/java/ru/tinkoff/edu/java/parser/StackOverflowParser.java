ea2dfea4b04e2a878a62305092c392f09c94f545
Fobiak <saysanov2018@mail.ru>
2023-03-28 03:06:49 +0500

    проверка ветки

--- link-parser/src/main/java/ru/tinkoff/edu/java/parser/StackOverflowParser.java
+++ link-parser/src/main/java/ru/tinkoff/edu/java/parser/StackOverflowParser.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public final class StackOverflowParser extends AbstractParser {
     @Override
     public UrlData parse(String url) {
-        if (url.startsWith("https://stackoverflow.com")) {
+        if (url.startsWith("hhttps://stackoverflow.com")) {
             String patternString = "^https?://stackoverflow\\.com/questions/(\\d+)/";
             Matcher matcher = Pattern.compile(patternString).matcher(url);
             return matcher.find() ? new StackOverflowData(Integer.parseInt(matcher.group(1))) : null;
